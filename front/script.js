0e1d95ab7040dfd0c134ef13547d13e8490933d4
Aislan <aislankaraujo@gmail.com>
2025-11-21 08:51:52 -0300

    Adiciona  recuperar senha e ajusta links de login

--- front/script.js
+++ front/script.js
@@ -11,4 +11,18 @@
                 toggleIcon.classList.remove('fa-eye-slash');
                 toggleIcon.classList.add('fa-eye');
             }
+        } 
+                function togglePasswordVisibility() {
+            const passwordField = document.getElementById('password');
+            const toggleIcon = document.querySelector('.password-toggle i');
+            
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                toggleIcon.classList.remove('fa-eye');
+                toggleIcon.classList.add('fa-eye-slash');
+            } else {
+                passwordField.type = 'password';
+                toggleIcon.classList.remove('fa-eye-slash');
+                toggleIcon.classList.add('fa-eye');
+            }
         }
