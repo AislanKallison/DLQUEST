a4f03bc163898b7b6506429063d17d19288c041d
Aislan <aislankaraujo@gmail.com>
2025-11-25 14:20:01 -0300

    atualização 24.11.2025 Cristef

--- front/script.js
+++ front/script.js
@@ -164,4 +164,22 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.getElementById('login-form')) {
         handleLogin();
     }
+}); 
+document.getElementById("signupForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    document.getElementById("successModal").style.display = "flex";
 });
+
+document.getElementById("okBtn").addEventListener("click", function () {
+    window.location.href = "login.html";
+});
+
+function togglePasswordVisibility(id) {
+    const input = document.getElementById(id);
+    if (input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+}
